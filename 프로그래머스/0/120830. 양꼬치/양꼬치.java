e58756ef12c427e8f9f62f3845654b5c86b75f13
muneo1008 <65271572+muneo1008@users.noreply.github.com>
2025-03-12 11:07:05 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int service = n / 10;
+        
+        answer = (12000*n) + ((k-service)*2000);
+        return answer;
+    }
+}
