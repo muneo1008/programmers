9fdffc80d93addeb28ec20868ebacf2c20974e9f
muneo1008 <65271572+muneo1008@users.noreply.github.com>
2025-03-21 07:35:17 +0900

    [level 2] Title: 기능개발, Time: 6.51 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        
+        ArrayList<Integer> days = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        int pre_day = 0;
+        
+        for(int i = 0; i < progresses.length; i++){
+            int remainProgresses = 100 - progresses[i];
+            int day = (remainProgresses % speeds[i] == 0) ? remainProgresses / speeds[i] : remainProgresses / speeds[i] + 1;
+            days.add(day);
+        }
+        
+        for(int j = 0; j < days.size(); j++){
+            int cnt = 1;
+            if(j == days.size() -1){
+                result.add(cnt);
+            }
+            for(int k = j+1; k < days.size(); k++){
+                if(days.get(j) < days.get(k)){
+                    result.add(cnt);
+                    j = k - 1;
+                    break;
+                }else{
+                    cnt++;
+                    if(k >= days.size() - 1){
+                        result.add(cnt);
+                        j = k;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(days);
+        System.out.println(result);
+        int[] answer = result.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
+    }
+}
